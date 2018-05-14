b689aac1cc6afafa8aa08f434ab3869bb146d55a
Ilmari Heikkinen <ilmari@fhtr.net>
2018-05-14 09:21:11 +0800

    streamtube integration scaffolding

---
+++ src/traces/streamtube/convert.js
@@ -0,0 +1,116 @@
+/**
+* Copyright 2012-2018, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+
+'use strict';
+
+var tube2mesh = require('gl-streamtube3d');
+var createMesh = tube2mesh.createTubeMesh;
+
+function Mesh3DTrace(scene, mesh, uid) {
+    this.scene = scene;
+    this.uid = uid;
+    this.mesh = mesh;
+    this.name = '';
+    this.color = '#fff';
+    this.data = null;
+    this.showContour = false;
+}
+
+var proto = Mesh3DTrace.prototype;
+
+proto.handlePick = function(selection) {
+    if(selection.object === this.mesh) {
+        var selectIndex = selection.index = selection.data.index;
+
+        selection.traceCoordinate = [
+            this.data.x[selectIndex],
+            this.data.y[selectIndex],
+            this.data.z[selectIndex]
+        ];
+
+        var text = this.data.text;
+        if(Array.isArray(text) && text[selectIndex] !== undefined) {
+            selection.textLabel = text[selectIndex];
+        } else if(text) {
+            selection.textLabel = text;
+        }
+
+        return true;
+    }
+};
+
+function zip3(x, y, z) {
+    var result = new Array(x.length);
+    for(var i = 0; i < x.length; ++i) {
+        result[i] = [x[i], y[i], z[i]];
+    }
+    return result;
+}
+
+function convert(scene, trace) {
+    var layout = scene.fullSceneLayout;
+
+    // Unpack position data
+    function toDataCoords(axis, coord, scale) {
+        return coord.map(function(x) {
+            return axis.d2l(x) * scale;
+        });
+    }
+
+    var params = {
+        startingPositions: [ [-0.2,-0.2,-0.2], [0.2,0.2,0.2] ],
+        meshgrid: [
+            toDataCoords(layout.xaxis, trace.cx, scene.dataScale[0]),
+            toDataCoords(layout.yaxis, trace.cy, scene.dataScale[1]),
+            toDataCoords(layout.zaxis, trace.cz, scene.dataScale[2])
+        ],
+        vectors: zip3(
+            toDataCoords(layout.xaxis, trace.u, scene.dataScale[0]),
+            toDataCoords(layout.yaxis, trace.v, scene.dataScale[1]),
+            toDataCoords(layout.zaxis, trace.w, scene.dataScale[2])
+        ),
+        colormap: 'portland'
+    };
+
+    var vectors = params.vectors;
+    params.vectors = params.vectors.concat(vectors);
+    params.vectors = params.vectors.concat(vectors);
+
+    var meshData = tube2mesh(
+        params,
+        [[-5, -5, -5], [5, 5, 5]]
+    );
+
+    return meshData;
+};
+
+proto.update = function(trace) {
+    var meshData = convert(trace);
+    this.mesh.update(meshData);
+};
+
+proto.dispose = function() {
+    this.scene.glplot.remove(this.mesh);
+    this.mesh.dispose();
+};
+
+function createMesh3DTrace(scene, trace) {
+    var gl = scene.glplot.gl;
+    var meshData = convert(scene, trace);
+    var mesh = createMesh(gl, meshData);
+    var result = new Mesh3DTrace(scene, mesh, trace.uid);
+    result.data = {hoverinfo: 'skip'};
+
+    mesh._trace = result;
+    scene.glplot.add(mesh);
+
+    return result;
+}
+
+module.exports = createMesh3DTrace;
